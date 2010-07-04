da611443542a70f166150246e63fccfa755ceec3
kamens <none@none>
2010-07-04 19:29:18 +0000

    Preserve linebreaks for questions and answers with ellipsis-hiding for questions taking up lots of space w/ lots of linebreaks
    --HG--
    extra : convert_revision : svn%3A60972f9f-2242-074b-9109-3933092f38a4/trunk%40145

--- discussion/templatefilters.py
+++ discussion/templatefilters.py
@@ -3,6 +3,7 @@ import re
 from google.appengine.ext import webapp
 from google.appengine.api import users
 from django import template
+from django.template.defaultfilters import linebreaksbr
 from django.template.defaultfilters import timesince
 
 import models
@@ -51,6 +52,28 @@ def youtube_timestamp_links(content):
 
     return content
 
+@register.filter
+def linebreaksbr_ellipsis(content):
+
+    # After a specified number of linebreaks, apply span with a CSS class
+    # to the rest of the content so it can be optionall hidden or shown
+    # based on its context.
+    max_linebreaks = 4
+
+    # We use django's built-in "linebreaksbr" filter, so we don't
+    # need to worry about alternate representations of the <br /> tag.
+    content = linebreaksbr(content.strip())
+
+    rg_s = re.split("<br />", content)
+    if len(rg_s) > (max_linebreaks + 1):
+        # More than max_linebreaks <br />'s were found.
+        # Place everything after the 3rd <br /> in a hidden span that can be exposed by CSS later, and
+        # Append an ellipsis at the cutoff point with a class that can also be controlled by CSS.
+        rg_s[max_linebreaks] = "<span class='ellipsisExpand'>&hellip;</span><span class='hiddenExpand'>" + rg_s[max_linebreaks]
+        rg_s[len(rg_s) - 1] += "</span>"
+
+    # Join the string back up w/ its original <br />'s
+    return "<br />".join(rg_s)
 
 @register.filter
 def timesince_ago(content):
