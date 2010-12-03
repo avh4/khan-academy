e31b43220cc451efdaab26dc15cbabbd7a2c12df
jmichelz <none@none>
2010-12-03 21:59:11 +0000

    removing --address=0.0.0.0, it gives problems with some computers
    --HG--
    extra : convert_revision : svn%3A60972f9f-2242-074b-9109-3933092f38a4/trunk%40778

--- offline/Khan Academy/run.bat
+++ offline/Khan Academy/run.bat
@@ -1,2 +1,3 @@
 rem --use_sqlite giving intermittent errors
-"%~dp0/code/Python25/python.exe" "%~dp0/code/google_appengine/dev_appserver.py" --address=0.0.0.0 --datastore_path="%~dp0/code/dev_appserver.datastore" "%~dp0/code/khanacademy-read-only"
+rem --address=0.0.0.0 gives problems with some computers
+"%~dp0/code/Python25/python.exe" "%~dp0/code/google_appengine/dev_appserver.py" --datastore_path="%~dp0/code/dev_appserver.datastore" "%~dp0/code/khanacademy-read-only"
