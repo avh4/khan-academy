dcd0f794bb7a1a0f96c4aff854f208d8465fd3c7
kamens <none@none>
2011-01-05 23:46:36 +0000

    gzip backup results before running a new one so we're sure to conserve space as early as possible.
    --HG--
    extra : convert_revision : svn%3A60972f9f-2242-074b-9109-3933092f38a4/trunk%40961

--- amazon/backup.sh
+++ amazon/backup.sh
@@ -3,9 +3,10 @@
 # Remove old bulkloader log files
 rm bulkloader-*
 
-# Backup entire datastore
-google_appengine/appcfg.py download_data --application=khanexercises --url=http://khanexercises.appspot.com/remote_api --filename=`date +%F.dat` --email=kamens@gmail.com --passin < private_pw > `date +%F.log`
-
 # Gzip results
 gzip *.dat
+gzip *.log
+
+# Backup entire datastore
+google_appengine/appcfg.py download_data --application=khanexercises --url=http://khanexercises.appspot.com/remote_api --filename=`date +%F.dat` --email=kamens@gmail.com --passin < private_pw > `date +%F.log`
 
