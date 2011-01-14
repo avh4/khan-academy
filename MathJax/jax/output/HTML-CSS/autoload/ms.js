207d799eed02d06598a4114ec54cecdae9e5005b
kamens <none@none>
2011-01-14 18:02:54 +0000

    Re-adding MathJax
    --HG--
    extra : convert_revision : svn%3A60972f9f-2242-074b-9109-3933092f38a4/trunk%401006

---
+++ MathJax/jax/output/HTML-CSS/autoload/ms.js
@@ -0,0 +1,16 @@
+/*
+ *  /MathJax/jax/output/HTML-CSS/autoload/ms.js
+ *  
+ *  Copyright (c) 2010 Design Science, Inc.
+ *
+ *  Part of the MathJax library.
+ *  See http://www.mathjax.org for details.
+ * 
+ *  Licensed under the Apache License, Version 2.0;
+ *  you may not use this file except in compliance with the License.
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+(function(a){var b="1.0";a.ms.Augment({toHTML:function(d){d=this.HTMLhandleSize(this.HTMLcreateSpan(d));var c=this.getValues("lquote","rquote");var f=this.data.join("");var e=[];if(c.lquote.length===1){e.push(this.HTMLquoteRegExp(c.lquote))}if(c.rquote.length===1){e.push(this.HTMLquoteRegExp(c.rquote))}if(e.length){f=f.replace(RegExp("("+e.join("|")+")","g"),"\\$1")}this.HTMLhandleVariant(d,this.HTMLgetVariant(),c.lquote+f+c.rquote);this.HTMLhandleSpace(d);this.HTMLhandleColor(d);return d},HTMLquoteRegExp:function(c){return c.replace(/([.*+?|{}()\[\]\\])/g,"\\$1")}});a.ms.prototype.defaults.fontfamily="monospace";MathJax.Hub.Startup.signal.Post("HTML-CSS ms Ready")})(MathJax.ElementJax.mml);MathJax.Ajax.loadComplete(MathJax.OutputJax["HTML-CSS"].autoloadDir+"/ms.js");
+
