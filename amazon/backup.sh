ea51072a4dd03412481c99a7d47b91ebeebc204b
kamens <none@none>
2011-02-02 19:01:43 +0000

    Backup script now copies gzip'd files to separate Amazon S3 bucket and deletes files more than 2 weeks old from micro EC2 instance.
    --HG--
    extra : convert_revision : svn%3A60972f9f-2242-074b-9109-3933092f38a4/trunk%401066

--- amazon/backup.sh
+++ amazon/backup.sh
@@ -10,3 +10,16 @@ gzip *.log
 # Backup entire datastore
 google_appengine/appcfg.py download_data --application=khanexercises --url=http://khanexercises.appspot.com/remote_api --filename=`date +%F.dat` --email=kamens@gmail.com --passin < private_pw > `date +%F.log`
 
+# Gzip results
+gzip *.dat
+gzip *.log
+
+for f in *.gz
+do
+  echo "Moving $f to s3 bucket"
+  s3cmd-1.0.0/s3cmd put $f s3://KA-datastore/$f
+done
+
+# Delete files older than 2 weeks
+find *.gz -mtime +14 -exec rm {} \;
+
