ac560cca045d0111e308ddb703808fb8c642bb1e
kamens <none@none>
2011-02-17 19:12:27 +0000

    Clarifying comment
    --HG--
    extra : convert_revision : svn%3A60972f9f-2242-074b-9109-3933092f38a4/trunk%401122

--- appengine_config.py
+++ appengine_config.py
@@ -1,6 +1,8 @@
 # Uncomment this function to enable AppStats performance recording:
 # http://code.google.com/appengine/docs/python/tools/appstats.html
 #
+# Once enabled, go to http://localhost:8080/_ah/stats/ for detailed perf stats.
+#
 # Do not leave uncommented unless actively profiling, because AppStats logging
 # incurs a slight performance penalty.
 #def webapp_add_wsgi_middleware(app):
