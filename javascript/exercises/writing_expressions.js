afb0c8e0bfeafa1414c0bc1f8a2168a134060a2a
Marcia Lee <marcia@khanacademy.org>
2011-03-17 14:51:45 -0700

    Vary order of clauses

--- javascript/exercises/writing_expressions.js
+++ javascript/exercises/writing_expressions.js
@@ -1,4 +1,4 @@
-// Translating Word Problems Into Equations
+// Writing Expressions 1
 // Author: Marcia Lee
 // Date: 2011-03-16
 //
@@ -6,7 +6,7 @@
 // Select the expression that represents the following phrase:
 //  b more/less than the product of a and x
 //  answer == ax + b or none of the above
-// Constraints: a and b are ints in (-10, 10) except 0
+// Constraints: a and b are ints in [-10, 10] except 0
 // 
 // 
 
@@ -15,8 +15,9 @@ function SimpleExpression() {
     var coeff = get_random();
     var variable = "x";
     var constant = get_random();
-    var numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-    var clauses = [];
+    var numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
+    var clauses = {};
+    var is_easier = get_random() > 5;
     
     this.toString = function() {
         return "" + format_first_coefficient(coeff) + variable + format_constant(constant);
@@ -25,24 +26,35 @@ function SimpleExpression() {
     this.toEnglish = function() {
         var result = "";
         var clause = "";
-        if (constant > 0)
-            clause += " " + numbers[constant] + " more than "
-        else if (constant < 0) {            
-            clause += " " + numbers[Math.abs(constant)] + " less than "
-        }
-
-        clauses.push(clause);
-        result += clause;
-        clause = "";
         
-        clause += " the product of "
+        if (!is_easier) {
+            clause += numbers[Math.abs(constant)]
+            if (constant > 0)
+                clause += " more than "
+            else if (constant < 0)
+                clause += " less than "
+            clauses.outer = clause;
+            result += clause;
+        }
+        
+        clause = " the product of "
         if (coeff < 0)
             clause += " negative ";
         clause += numbers[Math.abs(coeff)] + " and ";
         clause += " " + variable;
-        
-        clauses.push(clause);
+        clauses.inner = clause;
         result += clause;
+            
+        if (is_easier) {
+            clause = "";
+            if (constant > 0)
+                clause += " plus "
+            else
+                clause += " minus "
+            clause +=  numbers[Math.abs(constant)]
+            clauses.outer = clause;
+            result += clause;
+        }
         
         return result;
     }
@@ -51,17 +63,27 @@ function SimpleExpression() {
         write_step("Let's break this phrase into smaller parts.");
         
         open_left_padding(30);
-        var blue_clause = format_string_with_color(clauses[0], "blue");
-        var orange_clause = format_string_with_color(clauses[1], "orange");
-        write_step(blue_clause + orange_clause);
+        var outer_clause = format_string_with_color(clauses.outer, "blue");
+        var inner_clause = format_string_with_color(clauses.inner, "orange");
+        if (is_easier)
+            write_step(inner_clause + outer_clause);
+        else
+            write_step(outer_clause + inner_clause);
 
-        write_step("What is " + orange_clause + "?");
-        var orange_math = format_math_with_color(format_first_coefficient(coeff) + variable, "orange");
-        write_step("`" + coeff + " * " + variable + " = ` "+ orange_math);
+        write_step("What is " + inner_clause + "?");
+        var inner_expression = format_math_with_color(format_first_coefficient(coeff) + variable, "orange");
+        write_step("`" + coeff + " * " + variable + " = ` "+ inner_expression);
         
-        write_step("What is " + blue_clause + " " + orange_math + "?");
-        var blue_math = format_math_with_color(format_constant(constant), "blue");
-        write_step(orange_math + blue_math);
+        if (is_easier)
+            write_step("What is " + inner_expression + " " + outer_clause + "?");
+        else
+            write_step("What is " + outer_clause + " " + inner_expression + "?");
+        
+        var outer_expression = format_math_with_color(format_constant(constant), "blue");
+        
+        write_step(inner_expression + outer_expression);
+
+
         close_left_padding();
         
         write_step("So, " + this.toEnglish() + " can be written as "  + "`" + this.toString() + "`");
@@ -110,7 +132,7 @@ function WritingExpressionsExercise() {
         if (get_random() > -5) 
             setCorrectAnswer(expression.toString());
         else
-            setCorrectAnswer("`None of the above`");
+            setCorrectAnswer("`None of these`");
     }
     
     function showHints() {
@@ -119,7 +141,7 @@ function WritingExpressionsExercise() {
     
     function generateWrongAnswers() {
         // If None of the above is the correct answer, then the following has no effect.
-        addWrongChoice("`None of the above`");
+        addWrongChoice("`None of these`");
         
         var wrong = new SimpleExpression();
         wrong.setCoefficient(expression.getCoefficient() * -1);
