8d8f645299961bf220c69f381d1c4052d534844e
Marcia Lee <marcia@khanacademy.org>
2011-03-22 19:02:12 -0700

    Slight refactoring of writing expressions exercise

--- javascript/exercises/writing_expressions.js
+++ javascript/exercises/writing_expressions.js
@@ -4,126 +4,30 @@
 //
 // Problem layout:
 // Select the expression that represents the following phrase:
-//  b more/less than the product of a and x
+    
+    // b plus the product of a and x
+    // b plus the quantity a times x
+    
+    // the sum of b and the product of a and x
+    // the sum of b and the quantity a times x
+
+    // take the product of a and x, and then add/subtract b
+    // take the quantity a times x, and then add/subtract b
+    
 //  answer == ax + b or none of the above
 // Constraints: a and b are ints in [-10, 10] except 0
 // 
 // 
 
-// Expression of the form ax + b
-function Expression() {
-    var coeff = get_random();
-    var variable = "x";
-    var constant = get_random();
-    var numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
-    var clauses = {};
-    var is_easier = get_random() > 5;
-    
-    this.toString = function() {
-        return "" + format_first_coefficient(coeff) + variable + format_constant(constant);
-    }
-    
-    this.toEnglish = function() {
-        var result = "";
-        var clause = "";
-        
-        if (!is_easier) {
-            clause += numbers[Math.abs(constant)]
-            if (constant > 0)
-                clause += " more than "
-            else if (constant < 0)
-                clause += " less than "
-            clauses.outer = clause;
-            result += clause;
-        }
-        
-        clause = " the product of "
-        if (coeff < 0)
-            clause += " negative ";
-        clause += numbers[Math.abs(coeff)] + " and ";
-        clause += " " + variable;
-        clauses.inner = clause;
-        result += clause;
-            
-        if (is_easier) {
-            clause = "";
-            if (constant > 0)
-                clause += " plus "
-            else
-                clause += " minus "
-            clause +=  numbers[Math.abs(constant)]
-            clauses.outer = clause;
-            result += clause;
-        }
-        
-        return result;
-    }
-    
-    this.showHints = function() {
-        write_step("Let's break this phrase into smaller parts.");
-        
-        open_left_padding(30);
-        var outer_clause = format_string_with_color(clauses.outer, "blue");
-        var inner_clause = format_string_with_color(clauses.inner, "orange");
-        if (is_easier)
-            write_step(inner_clause + outer_clause);
-        else
-            write_step(outer_clause + inner_clause);
-
-        write_step("What is " + inner_clause + "?");
-        var inner_expression = format_math_with_color(format_first_coefficient(coeff) + variable, "orange");
-        write_step("`" + coeff + " * " + variable + " = ` "+ inner_expression);
-        
-        if (is_easier)
-            write_step("What is " + inner_expression + " " + outer_clause + "?");
-        else
-            write_step("What is " + outer_clause + " " + inner_expression + "?");
-        
-        var outer_expression = format_math_with_color(format_constant(constant), "blue");
-        
-        write_step(inner_expression + outer_expression);
-
-
-        close_left_padding();
-        
-        write_step("So, " + this.toEnglish() + " can be written as "  + "`" + this.toString() + "`");
-    }
-    
-    this.getCoefficient = function() {
-        return coeff;
-    }
-    
-    this.getConstant = function() {
-        return constant;
-    }
-    
-    this.getVariable = function() {
-        return variable;
-    }
-    
-    this.setCoefficient = function(new_coeff) {
-        coeff = new_coeff;
-    }
-    
-    this.setConstant = function(new_constant) {
-        constant = new_constant;
-    }
-    
-    this.equals = function (other_expression) {
-        return coeff == other_expression.getCoefficient() && constant == other_expression.getConstant();
-    }
-}
-
 function WritingExpressionsExercise() {
-    var expression;
+    var expression = new Expression();
     
     showProblem();
     showHints();
     generateWrongAnswers();
     
     function showProblem() {
-        expression = new Expression();
-        write_text("Select the expression that represents the following phrase: ");
+        write_text("Select the expression that matches the following phrase: ");
         open_left_padding(30);
         write_text(expression.toEnglish());
         close_left_padding();
@@ -136,7 +40,13 @@ function WritingExpressionsExercise() {
     }
     
     function showHints() {
+        write_step("Let's break this problem into smaller and easier pieces.");
+        
+        open_left_padding(30);
         expression.showHints();
+        close_left_padding();
+        
+        write_step("So, the original phrase can be written as "  + "`" + expression.toString() + "`");
     }
     
     function generateWrongAnswers() {
