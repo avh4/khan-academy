975a3f97f7e168418180249bdec1e3ba028457aa
Ben Kamens <ben@khanacademy.org>
2011-04-12 18:36:31 -0400

    Get rid of the 'must do a problem or watch a video' requirement for tenure badges. Hand 'em out like candy if we have to, they might bring lost users back.

--- badges/tenure_badges.py
+++ badges/tenure_badges.py
@@ -10,32 +10,17 @@ class TenureBadge(Badge):
 
     def is_satisfied_by(self, *args, **kwargs):
         user_data = kwargs.get("user_data", None)
-        action_cache = kwargs.get("action_cache", None)
-        if user_data is None or action_cache is None:
+        if user_data is None:
             return False
 
         # Make sure they've been a member for at least X years
         if user_data.joined is None or util.seconds_since(user_data.joined) < self.seconds_required:
             return False
 
-        # Make sure they've seen recent activity of any sort in the past 30 days
-        c_problem_logs = len(action_cache.problem_logs)
-        c_video_logs = len(action_cache.video_logs)
-
-        if c_problem_logs > 0:
-            problem_log = action_cache.get_problem_log(c_problem_logs - 1)
-            if util.seconds_since(problem_log.time_done) < 60 * 60 * 24 * 30:
-                return True
-
-        if c_video_logs > 0:
-            video_log = action_cache.get_video_log(c_video_logs - 1)
-            if util.seconds_since(video_log.time_watched) < 60 * 60 * 24 * 30:
-                return True
-
-        return False
+        return True
 
     def extended_description(self):
-        return "Remain an active member of the Khan Academy for %s" % util.seconds_to_time_string(self.seconds_required)
+        return "Remain a member of the Khan Academy for %s" % util.seconds_to_time_string(self.seconds_required)
 
 class YearOneBadge(TenureBadge):
     def __init__(self):
