67c893d1d71d3a58fb98aef47cc0925beec6bc5b
Marcia Lee <marcia@khanacademy.org>
2011-04-22 23:22:56 -0700

    Trickier numbers for sig fig 1

--- javascript/exercises/significant_figures_1.js
+++ javascript/exercises/significant_figures_1.js
@@ -11,7 +11,6 @@
 //  In other words, if there is no decimal, trailing zeros are not significant
 //
 
-
 function SigFig1Exercise() {
     var number_str = "";
     var answer;
@@ -20,54 +19,67 @@ function SigFig1Exercise() {
     var index_decimal;
     
     generateProblem();
-    showProblem();
-
+    
     function generateProblem() {
-        var num_leading_zeros = getRandomIntRange(0, 2) ? 0 : getRandomIntRange(1, 2);
-        var num_trailing_zeros = getRandomIntRange(0, 2) ? 0 : getRandomIntRange(1, 2);
-
-        // 0.000X is tricky, so let's throw this in 1/6 of the time.
-        if (!getRandomIntRange(0, 5)) {
-            num_leading_zeros = 5;
-            num_trailing_zeros = 0;
-        }
-        
-        var num_between = 6 - num_leading_zeros - num_trailing_zeros;
-        
-        for (var i = 0; i < num_leading_zeros; i++)
-            number_str += "0";
-
-        for (var i = 0; i < num_between; i++)
-            number_str += !getRandomIntRange(0, 2) ? 0 : getRandomIntRange(1, 9);
-            
-        for (var i = 0; i < num_trailing_zeros; i++)
-            number_str += "0";
-            
-        var decimal = 0;
-        if (number_str[0] == "0")
-            decimal = 1;
+        if (getRandomIntRange(0, 5))
+            number_str = generateNormalNumber();
         else
-            decimal = getRandomIntRange(-3, number_str.length);
+            number_str = generateZeroNumber();
             
-        if (decimal >= 0)
-            number_str = number_str.substring(0, decimal) + "." + number_str.substring(decimal);
-        
-        // Right now, don't allow for 0.00000 :(
-        if (number_str.search(/[1-9]/) == -1) {
-            number_str = number_str.substring(0, number_str.length - 1) + getRandomIntRange(1, 9);
-        }
-            
-        index_first_non_zero = 0;
-        index_last_non_zero = number_str.length - 1;
         answer = countNumSigFig(number_str);
         setCorrectAnswer(answer);
-    }
-    
-    function showProblem() {
         write_text("How many significant figures does " + number_str + " have?");
+    }
+
+    function generateNormalNumber() {
+        var result = "";
+        var num_leading_zeros = getRandomIntRange(0, 2) ? 0 : getRandomIntRange(1, 2);
+        var num_trailing_zeros = getRandomIntRange(0, 2) ? 0 : getRandomIntRange(1, 2);
+        var num_between = 6 - num_leading_zeros - num_trailing_zeros;
+        var decimal;
+        
+        for (var i = 0; i < num_leading_zeros; i++)
+            result += "0";
+        for (var i = 0; i < num_between; i++)
+            result += !getRandomIntRange(0, 2) ? 0 : getRandomIntRange(1, 9);
+        for (var i = 0; i < num_trailing_zeros; i++)
+            result += "0";
+            
+        if (result[0] == "0")
+            decimal = 1;
+        else
+            decimal = getRandomIntRange(-3, result.length);
+            
+        if (decimal >= 0)
+            result = result.substring(0, decimal) + "." + result.substring(decimal);
+            
+        return result;
+    }
+
+    function generateZeroNumber() {
+        // To generate the trickier numbers like 0.00 or just plain old 0
+        var result = "0";
+        var num_zeros = getRandomIntRange(1, 3);
+        if (num_zeros != 1) {
+            result += ".";
+            for (var i = 1; i < num_zeros; i++)
+                result += "0";
+        }
+        return result;
     }
     
     function countNumSigFig(str) {
+        index_first_non_zero = 0;
+        index_last_non_zero = number_str.length - 1;
+        index_decimal = str.indexOf(".");
+        // Special case if the entire number is zeros
+        if (number_str.search(/[1-9]/) == -1) {
+            if (index_decimal == -1)
+                return 1;
+            else
+                return number_str.length - index_decimal;
+        }
+        
         // Find first non zero char (to ignore leading zeros)
         for (; index_first_non_zero < str.length; index_first_non_zero++) {
             var c = str[index_first_non_zero];
@@ -83,7 +95,6 @@ function SigFig1Exercise() {
         }
 
         // If there is a decimal, count trailing zeros
-        index_decimal = str.indexOf(".");
         if (index_decimal == -1)
             return index_last_non_zero - index_first_non_zero + 1;
         else if (index_decimal > index_first_non_zero)
@@ -102,56 +113,91 @@ function SigFig1Exercise() {
     
     function highlightNum(start, end, color) {
         for (var i = start; i < end; i++) {
-            if (i == index_decimal) continue;
-            raphael_numbers[i].attr({"font-size": 30, "stroke": color, "fill": color});
+            if (i != index_decimal) 
+                raphael_numbers[i].attr({"font-size": 30, "stroke": color, "fill": color});
         }
     }
 
+    function showNumber() {
+        for (var i = 0; i < number_str.length; i++) {
+            raphael_numbers[raphael_numbers.length] = present.paper.text(10 + i * 20, 20, number_str[i]).attr({"font-size": 30});
+        }        
+    }
+    
+    function getSignificantHint() {
+        var hint;
+        if (number_str.search(/[1-9]/) == -1) {
+            if (index_decimal == -1) {
+                hint = "In this special case with only zeros and no decimal, there is 1 significant figure.";
+                highlightNum(number_str.length - 1, number_str.length, color_sig);
+            } else {
+                hint = "In this special case with only zeros and a decimal, all " + answer + " zeros are significant.";
+                raphael_numbers[index_decimal].attr({"font-size": 30, "stroke": color_decimal, "fill": color_decimal}); 
+                highlightNum(0, number_str.length, color_sig);
+            }
+            optional_hints_given[0] = true;
+            optional_hints_given[1] = true;
+        } else {
+            hint = "Identify the non-zero digits and any zeros between them. These are all significant.";
+            highlightNum(index_first_non_zero, index_last_non_zero + 1, color_sig);                    
+        }
+        return hint;
+    }
+    
+    function getLeadingHint() {
+        highlightNum(0, index_first_non_zero, color_leading);
+        return "Leading zeros are not significant.";
+    }
+    
+    function getTrailingHint() {
+        var hint;
+        if (index_decimal == -1) {
+            hint = "Since there is no decimal, trailing zeros are not significant."
+            color = color_trailing;                        
+        } else {
+            hint = "Since there is a decimal, trailing zeros are significant."
+            color = color_sig;  
+            raphael_numbers[index_decimal].attr({"font-size": 30, "stroke": color_decimal, "fill": color_decimal});   
+        }
+        highlightNum(index_last_non_zero + 1, number_str.length, color);
+        optional_hints_given[0] = true;
+        return hint;
+    }
+    
+    function getFinalHint() {
+        optional_hints_given[1] = true;
+        return number_str + " has " + answer + " significant figure" + (answer == 1 ? "" : "s") + ".";
+    }
+    
     this.showGraphHints = function(hint_num) {
         var hint = "";
-        var paper = present.paper;
 
         switch (hint_num) {
             case 0:
-                for (var i = 0; i < number_str.length; i++) {
-                    raphael_numbers[raphael_numbers.length] = paper.text(10 + i * 20, 20, number_str[i]).attr({"font-size": 30});
-                }
+                showNumber();
                 break;
             case 1:
-                hint = "Identify the non-zero digits and any zeros between them. These are all significant.";
-                highlightNum(index_first_non_zero, index_last_non_zero + 1, color_sig);
+                hint = getSignificantHint();
                 break;
             case 2:
                 if (index_first_non_zero) {
-                    hint = "Leading zeros are not significant.";
-                    highlightNum(0, index_first_non_zero, color_leading);
+                    hint = getLeadingHint();
                     break;
                 }
             case 3:
-                if (index_last_non_zero + 1 != number_str.length && !optional_hints_given[0]){
-                    if (index_decimal == -1) {
-                        hint = "Since there is no decimal, trailing zeros are not significant."
-                        color = color_trailing;                        
-                    } else {
-                        hint = "Since there is a decimal, trailing zeros are significant."
-                        color = color_sig;  
-                        raphael_numbers[index_decimal].attr({"font-size": 30, "stroke": color_decimal, "fill": color_decimal});   
-                    }
-                    highlightNum(index_last_non_zero + 1, number_str.length, color);
-                    optional_hints_given[0] = true;
-                    break;                   
+                if (index_last_non_zero != number_str.length - 1 && !optional_hints_given[0]){
+                    hint = getTrailingHint();
+                    break;
                 }
             case 4:
                 if (!optional_hints_given[1]) {
-                    hint = number_str + " has " + answer + " significant figure" + (answer == 1 ? "" : "s") + ".";
-                    optional_hints_given[1] = true;
-                    break;                    
+                    hint = getFinalHint();
                 }
             default:
         }
 
         if (hint_num > 0 && hint_num < 5 && !done_hinting) {
-            paper.text(10, 30 + (hint_num) * 30, hint).attr({"font-size": 15, "text-anchor": "start"});
+            present.paper.text(10, 30 + (hint_num) * 30, hint).attr({"font-size": 15, "text-anchor": "start"});
             if (optional_hints_given[1])
                 done_hinting = true;
         }
