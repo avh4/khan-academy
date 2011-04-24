1be90e3b31192cbeaa20add741cd4cc211193a4c
Ben Kamens <ben@khanacademy.org>
2011-04-24 12:38:19 -0400

    First step in transition to nicer content-hashable JS/CSS packaging system

---
+++ js_css_packages/packages.py
@@ -0,0 +1,70 @@
+javascript = {
+    "shared": {
+        "hashed-filename": "hash-@shared-js@.js", # Will be replaced by deploy script
+        "files": [
+            "jquery.js",
+            "jquery-ui.js",
+            "jquery.ui.menu.js",
+            "jquery.watermark.js",
+            "jquery.placeholder.js",
+            "discussion.js",
+            "pageutil.js",
+        ]
+    },
+    "homepage": {
+        "hashed-filename": "hash-@homepage-js@.js", # Will be replaced by deploy script
+        "files": [
+            "jquery.easing.1.1.1.js",
+            "jquery.cycle.all.min.js",
+            "waypoints.min.js",
+            "homepage.js",
+        ]
+    },
+    "exercises": {
+        "hashed-filename": "hash-@exercises-js@.js", # Will be replaced by deploy script
+        "files": [
+            "ASCIIMathML.js",
+            "raphael.js",
+            "ASCIIsvg-wrapper.js",
+            "fontdetect.js",
+            "seedrandom.js",
+            "metautil.js",
+            "exerciseutil.js",
+            "graphutil.js",
+            "g.raphael-min.js",
+            "g.pie-min.js",
+            "g.line-min.js",
+        ]
+    },
+    "profile": {
+        "hashed-filename": "hash-@profile-js@.js", # Will be replaced by deploy script
+        "files": [
+            "jquery.address-1.3.1.min.js",
+            "highcharts.js",
+            "profile.js",
+        ]
+    },
+    "maps": {
+        "hashed-filename": "hash-@maps-js@.js", # Will be replaced by deploy script
+        "files": [
+            "fastmarkeroverlay.js",
+            "knowledgemap.js",
+        ]
+    },
+
+}
+
+stylesheets = {
+    "shared": {
+        "hashed-filename": "hash-@shared-css@.js", # Will be replaced by deploy script
+        "files": [
+            "default.css",
+            "rating.css",
+            "stylesheet.css",
+            "discussion.css",
+            "menu.css",
+            "profile.css",
+            "jquery-ui-1.8.4.custom.css",
+        ]
+    }
+}
---
+++ js_css_packages/templatetags.py
@@ -0,0 +1,32 @@
+from google.appengine.ext import webapp
+
+from app import App
+from js_css_packages import packages
+
+register = webapp.template.create_template_register()
+
+@register.simple_tag
+def js_package(package_name):
+    package = packages.javascript[package_name]
+    src_dir = "/javascript/%s-package" % package_name
+
+    if App.is_dev_server:
+        list_js = []
+        for filename in package["files"]:
+            list_js.append("<script type='text/javascript' src='%s/%s'></script>" % (src_dir, filename))
+        return "".join(list_js)
+    else:
+        return "<script type='text/javascript' src='%s/%s'></script>" % (src_dir, package["hashed-filename"])
+
+@register.simple_tag
+def css_package(package_name):
+    package = packages.stylesheets[package_name]
+    src_dir = "/stylesheets/%s-package" % package_name
+
+    if App.is_dev_server:
+        list_css = []
+        for filename in package["files"]:
+            list_css.append("<link rel='stylesheet' type='text/css' href='%s/%s'/>" % (src_dir, filename))
+        return "".join(list_css)
+    else:
+        return "<link rel='stylesheet' type='text/css' href='%s/%s'/>" % (src_dir, package["hashed-filename"])
