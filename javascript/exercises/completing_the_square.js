74d6dfa12c5647a6d18899059ea5ca3b6a3c562e
Marcia Lee <marcia@khanacademy.org>
2011-05-06 17:10:57 -0700

    Change completing the square to free answer

--- javascript/exercises/completing_the_square.js
+++ javascript/exercises/completing_the_square.js
@@ -26,14 +26,8 @@ function CompletingTheSquareExercise() {
         x2 = -a + c;
         
         // present smaller number first
-        var temp;
-        if (x2 < x1) {
-            temp = x1;
-            x1 = x2;
-            x2 = temp;
-        }
-            
-        setCorrectAnswer(formatAnswer(x1, x2));
+        x1 = Math.min(x1, x2);
+        x2 = Math.max(x1, x2);
         
         if (getRandomIntRange(1, 10) <= 4) {
             leadingCoefficient = getRandomIntRange(1, 6);
@@ -41,34 +35,19 @@ function CompletingTheSquareExercise() {
                 leadingCoefficient *= -1;
             }
         }
-        
-        while (Exercise.getNumPossibleAnswers() < 4) {
-            var fake_c = nonZeroRandomInt(-12, 12);
-            var f1 = -a - fake_c;
-            var f2 = -a + fake_c;
-            // smaller number first, again
-            if (f2 < f1) {
-                temp = f1;
-                f1 = f2;
-                f2 = temp;
-            }
-            addWrongChoice(formatAnswer(f1, f2));
-        }
-    }
-    
-    function formatAnswer(first, second) {
-        return "x = " + first + " and " + " x = " + second;
     }
     
     function showProblem() {
-        write_text("Solve the following quadratic equation by completing the square:");
+        write_text("Complete the square:");
         write_text(equation_string(format_first_coefficient(leadingCoefficient) + "x^2 " + format_coefficient(2 * a * leadingCoefficient) + "x " + format_constant(n * leadingCoefficient) + " = 0"));
+        write_text("To answer this question, provide the values of `a` and `b` (not of `x`).");
+        write_text(equation_string("(x + a)^2 = b^2"));
     }
     
     function generateHints() {
         open_left_padding(30);
-        
-        write_step("Remember, completing the square is about getting the equation to this form:" + equation_string("(x + a)^2 = x^2 + 2ax + a^2"));
+
+        write_step("Notice the following: " + equation_string("(x + a)^2 = x^2 + 2ax + a^2"));
         if (leadingCoefficient != 1) {
             write_step("Divide the entire equation by `" + leadingCoefficient + "` to get " + equation_string("x^2 " + format_coefficient(2 * a) + "x " + format_constant(n) + " = 0"));
         }
@@ -79,12 +58,15 @@ function CompletingTheSquareExercise() {
             phrase = "Add `" + -n + "` to ";
         }
         if (n != 0)
-            write_step(phrase + "both sides, resulting in this equation:" + equation_string("x^2 " + format_coefficient(2 * a) + "x = " + -n));
-        write_step("In the problem above, `2ax = " + (2 * a) + "x`. Therefore, `a = (" + (2 * a) + "x)/(2x) = " + a + "`.");
-        write_step("Add `a^2` to both sides, giving:" + equation_string("x^2 " + format_coefficient(2 * a) + "x + " + a * a + " = " + ((a * a) - n)));
-        write_step("Rewrite the problem as:" + equation_string("(x " + format_constant(a) + ")^2 = " + ((a * a) - n)) + "Get the square root of both sides, and solve for x.");
+            write_step(phrase + "both sides of the original equation:" + equation_string("x^2 " + format_coefficient(2 * a) + "x = " + -n));
+        write_step("We see that `2ax ` corresponds with `" + (2 * a) + "x`. Therefore, `2ax = " + (2 * a) + "x `" 
+                    + " and  " + "`a = (" + (2 * a) + "x)/(2x) = " + a + "`.");
+        write_step("Add `a^2` or `" + (a * a) + "` to both sides:" + equation_string("x^2 " + format_coefficient(2 * a) + "x + " + a * a + " = " + ((a * a) - n)));
+        write_step("This can be rewritten as:" + equation_string("(x " + format_constant(a) + ")^2 = " + ((a * a) - n)));
+        write_step("So, `a = " + a + "` and `b = " + c + "`.");
+        write_step("To solve for x, take the square root of both sides.");
         write_step(equation_string("x " + format_constant(a) + " = +-" + Math.abs(c)));
-        write_step(equation_string("x = " + x1 + " and x = " + x2))
+        write_step(equation_string("x = " + x1 + " and x = " + x2));
         
         close_left_padding();
     }
@@ -92,4 +74,20 @@ function CompletingTheSquareExercise() {
     generateProblem();
     showProblem();
     generateHints();
+    
+    this.checkAnswer = function() {
+        var result_a = isInputCorrect($("#answer_a").val(), a, 0);
+        var result_b = isInputCorrect($("#answer_b").val(), c, 0);
+        
+        if (result_a == Answer.INVALID || result_b == Answer.INVALID) {
+            $("#answer_a").focus();
+            return Answer.INVALID;            
+        }
+
+        if (result_a == Answer.CORRECT && result_b == Answer.CORRECT)
+            return Answer.CORRECT;
+            
+        $("#answer_a").focus();
+        return Answer.INCORRECT;
+    }
 }
