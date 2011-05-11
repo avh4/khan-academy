bb98ae60e08c62fbfa86d6c04774630f6d0f4e1d
Ben Kamens <ben@khanacademy.org>
2011-05-11 13:22:58 -0700

    Two stage render decorator

--- two_stage_template/__init__.py
+++ two_stage_template/__init__.py
@@ -1,13 +1,20 @@
+import logging
+
 import request_handler
 from two_stage_template import monkey_patches
 
+def two_stage_render():
+    def decorator(target):
+        def wrapper(*args, **kwargs):
+            monkey_patches.patch()
+            return target(*args, **kwargs)
+        return wrapper
+    return decorator
+
 class TwoStageTest(request_handler.RequestHandler):
 
-    #@two_stage_test
+    @two_stage_render()
     def get(self):
-
-        monkey_patches.patch()
-
         template_values = {
             "sheep": 1,
             "monkey": "ooh ooh aah aah",
