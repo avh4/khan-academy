b7f7ca129fa570c45ec7f76e857d97a4048127bc
Ben Kamens <ben@khanacademy.org>
2011-05-11 23:46:51 -0700

    Cleaner context caching w/ TwoPassVariableContext

--- two_stage_template/__init__.py
+++ two_stage_template/__init__.py
@@ -20,25 +20,30 @@ def two_pass_handler():
         return wrapper
     return decorator
 
+class TwoPassVariableContext:
+    def __init__(self, target, args):
+        self.target_name = target.__name__
+        self.args = args
+
+    def prepend_args(self, args):
+        args = list(args)
+        args.extend(self.args)
+        self.args = args
+
 def two_pass_variable():
     def decorator(target):
         def wrapper(*args, **kwargs):
             first_pass_call = kwargs.get("first_pass_call", True)
 
             if first_pass_call:
-                def inner_wrapper(*args_throwaway, **kwargs_throwaway):
-                    logging.critical("setting")
-                    memcache.set("some magic key", args[1:], namespace=App.version)
-                    return target(*args)
-                inner_wrapper.target_name = target.__name__
-                return inner_wrapper
+                variable_context = TwoPassVariableContext(target, args[1:])
+                memcache.set("5some magic key", variable_context, namespace=App.version)
+                return variable_context
             else:
                 def inner_wrapper(*args, **kwargs):
-                    args_cached = memcache.get("some magic key", namespace=App.version)
-                    logging.critical("getting: %s" % args_cached)
-                    args = list(args)
-                    args.extend(args_cached)
-                    return target(*args)
+                    variable_context = memcache.get("5some magic key", namespace=App.version)
+                    variable_context.prepend_args(args)
+                    return target(*variable_context.args)
                 return inner_wrapper
 
         return wrapper
@@ -75,13 +80,12 @@ class TwoPassTemplate():
         template_name, template_values = target(handler)
         template_value_fxn_names = {}
 
-        # Remove callable keys for first pass render,
+        # Remove two pass variable contexts for first pass render,
         # and keep references around for second pass
         for key in template_values.keys():
-            if callable(template_values[key]):
-                wrapped_fxn = template_values[key]
-                wrapped_fxn()
-                template_value_fxn_names[key] = wrapped_fxn.target_name
+            val = template_values[key]
+            if isinstance(val, TwoPassVariableContext):
+                template_value_fxn_names[key] = val.target_name
                 del template_values[key]
 
         return (handler.render_template_to_string(template_name, template_values), template_value_fxn_names)
