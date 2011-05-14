8881ae69bc9ff8e314ebf0219df767435c9c0b13
Ben Kamens <ben@khanacademy.org>
2011-05-13 19:46:06 -0700

    More complex unit tests
    --HG--
    rename : two_pass_template/tests/two_pass_test.html => two_pass_template/tests/test_complex.html

--- two_pass_template/tests/__init__.py
+++ two_pass_template/tests/__init__.py
@@ -1,22 +1,11 @@
 import logging
+import string
 import StringIO
 
 from two_pass_template import two_pass_variable, two_pass_handler
 import request_handler
 
 class TwoPassTemplateTest(request_handler.RequestHandler):
-
-    @two_pass_variable()
-    def sheep(self, monkey):
-        return monkey + self.request_int("inc", default=1)
-
-    @two_pass_variable()
-    def donkey(self, gorilla):
-        return gorilla + self.request_int("inc", default=1)
-
-    @two_pass_variable()
-    def zebras(self):
-        return "hrm"
 
     @two_pass_handler()
     def test_simple(self):
@@ -37,6 +26,18 @@ class TwoPassTemplateTest(request_handler.RequestHandler):
         }
         return ("two_pass_template/tests/test_unsafe.html", template_values)
 
+    @two_pass_variable()
+    def sheep(self, monkey):
+        return monkey + 1
+
+    @two_pass_variable()
+    def donkey(self, gorilla):
+        return gorilla + 1
+
+    @two_pass_variable()
+    def zebras(self):
+        return "hrm"
+
     @two_pass_handler()
     def test_complex(self):
 
@@ -47,35 +48,88 @@ class TwoPassTemplateTest(request_handler.RequestHandler):
             "sheep": self.sheep(monkey),
             "donkey": self.donkey(gorilla),
             "zebras": self.zebras(),
-            "monkey": "ooh ooh aah aah",
+            "monkey": "on trees",
             "monkeys": ["a", "b"],
             "unsafe_string": "unsafe_{{monkey}}"
         }
 
-        return ("two_pass_template/two_pass_test.html", template_values)
+        return ("two_pass_template/tests/test_complex.html", template_values)
+
+    @staticmethod
+    def assert_equal_no_whitespace(result, expected):
+        for c in string.whitespace:
+            result = result.replace(c, "")
+            expected = expected.replace(c, "")
+        assert(result == expected)
 
     def get(self):
 
         self.response.out = StringIO.StringIO()
         self.test_simple(first_pass_override=True)
         result = self.response.out.getvalue().strip()
-        assert(result == "hello gorilla goodbye")
+        self.assert_equal_no_whitespace(result, "hello gorilla goodbye")
 
         self.response.out = StringIO.StringIO()
         self.test_simple()
         result = self.response.out.getvalue().strip()
-        assert(result == "hello gorilla goodbye")
+        self.assert_equal_no_whitespace(result, "hello gorilla goodbye")
 
         self.response.out = StringIO.StringIO()
         self.test_unsafe()
         result = self.response.out.getvalue().strip()
-        assert(result == "&#123;&#123;&lt;script&gt;&#125;&#125;&lt;script&gt;")
+        self.assert_equal_no_whitespace(result, "&#123;&#123;&lt;script&gt;&#125;&#125;&lt;script&gt;")
 
         self.response.out = StringIO.StringIO()
         self.test_unsafe(first_pass_override=True)
         result = self.response.out.getvalue().strip()
-        assert(result == "&#123;&#123;&lt;script&gt;&#125;&#125;{{unsafe_string_two|escape}}")
+        self.assert_equal_no_whitespace(result, "&#123;&#123;&lt;script&gt;&#125;&#125;{{unsafe_string_two|escape}}")
+
+        self.response.out = StringIO.StringIO()
+        self.test_complex(first_pass_override=True)
+        result = self.response.out.getvalue().strip()
+        expected = """
+            <html>
+                <body>
+                    unsafe_&#123;&#123;monkey&#125;&#125;
+                    <script>var test = 'unsafe_\u007B\u007Bmonkey\u007D\u007D';</script>
+
+                    {%if sheep%}
+                        Test({{sheep}} vs. {{donkey}})
+                        {%if monkeys%}
+                            {%for a in monkeys%}
+                                Grr?! {{monkey}}
+                            {%endfor%}
+                        {%endif%}
+                        Nope
+                    {%endif%}
+
+                    
+                        {{zebras}}
+                    
+
+                </body>
+            </html>
+        """
+        self.assert_equal_no_whitespace(result, expected)
+
+        self.response.out = StringIO.StringIO()
+        self.test_complex()
+        result = self.response.out.getvalue().strip()
+        expected = """
+            <html>
+                <body>
+                    unsafe_&#123;&#123;monkey&#125;&#125;
+                    <script>var test = 'unsafe_\u007B\u007Bmonkey\u007D\u007D';</script>
+                        Test(6 vs. 7)
+                                Grr?! on trees
+                                Grr?! on trees
+                        Nope
+                        hrm
+                </body>
+            </html>
+        """
+        logging.critical(result)
+        self.assert_equal_no_whitespace(result, expected)
 
         self.response.out = StringIO.StringIO()
         self.response.out.write("All tests passed.")
-
