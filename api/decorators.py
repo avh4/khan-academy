945fe53ae39a05bf9909435aa6f75bd334e8c399
Ben Kamens <ben@khanacademy.org>
2011-05-18 11:37:58 -0700

    Return proper application/json response type

--- api/decorators.py
+++ api/decorators.py
@@ -2,16 +2,18 @@ import simplejson
 
 from functools import wraps
 from flask import request
+from flask import current_app
 
 def jsonp(func):
     @wraps(func)
     def jsonp_enabled(*args, **kwargs):
         obj = func(*args, **kwargs)
-        json = obj if type(obj) == str else simplejson.dumps(obj, ensure_ascii=False, indent=4)
+        json = obj if type(obj) == str else simplejson.dumps(obj, ensure_ascii=False, indent=None if request.is_xhr else 4)
 
+        val = json
         callback = request.values.get("callback")
         if callback:
-            return "%s(%s)" % (callback, json)
-        else:
-            return json
+            val = "%s(%s)" % (callback, json)
+
+        return current_app.response_class(val, mimetype="application/json")
     return jsonp_enabled
