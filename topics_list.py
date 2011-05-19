2fc8f65e17d0ff926a048f9265516e698145d189
Ben Kamens <ben@khanacademy.org>
2011-05-18 23:28:06 -0700

    Switching structure of playlist categories to more re-usable data structure so we can use it for playlist browser and hand it out to API.

--- topics_list.py
+++ topics_list.py
@@ -1,5 +1,86 @@
-import copy
+import logging
 
+# Each leaf node in PLAYLIST_STRUCTURE is a tuple of form:
+#   ("<Playlist short description for jump bar>", "<Playlist title>", "<Jump bar URL override (optional)>")
+
+PLAYLIST_STRUCTURE = {
+    "Math": [
+        ("Arithmetic", "Arithmetic"),
+        {
+            "Developmental Math": [
+                ("Developmental Math 1", "Developmental Math"),
+                ("Developmental Math 2", "Developmental Math 2"),
+            ]
+        },
+        {
+            "Pre-Algebra": [
+                ("Core Pre-Algebra", "Pre-algebra"),
+                ("Worked Examples 1", "MA Tests for Education Licensure (MTEL) -Pre-Alg"),
+            ]
+        },
+        ("Brain Teasers", "Brain Teasers"),
+        {
+            "Algebra": [
+                ("Core Algebra", "Algebra"),
+                ("Worked Examples 1", "California Standards Test: Algebra I"),
+                ("Worked Examples 2", "ck12.org Algebra 1 Examples"),
+                ("Worked Examples 3", "California Standards Test: Algebra II"),
+                ("Worked Examples 4", "Algebra I Worked Examples"),
+            ]
+        },
+        {
+            "Geometry": [
+                ("Core Geometry", "Geometry"),
+                ("Worked Examples 1", "California Standards Test: Geometry"),
+            ]
+        },
+        ("Trigonometry", "Trigonometry"),
+        ("Probability", "Probability"),
+        ("Statistics", "Statistics"),
+        ("Precalculus", "Precalculus"),
+        ("Calculus", "Calculus"),
+        ("Differential Equations", "Differential Equations"),
+        ("Linear Algebra", "Linear Algebra"),
+    ],
+    "Science": [
+        ("Biology", "Biology"),
+        ("Chemistry", "Chemistry"),
+        ("Physics", "Physics"),
+        ("Organic Chemistry", "Organic Chemistry"),
+        ("Cosmology and Astronomy", "Cosmology and Astronomy"),
+    ],
+    "Humanities & Other": [
+        ("History", "History"),
+        {
+            "Finance": [
+                ("Core Finance", "Finance"),
+                ("Banking and Money", "Banking and Money"),
+                ("Valuation and Investing", "Valuation and Investing"),
+                ("Venture Capital and Capital Markets", "Venture Capital and Capital Markets"),
+                ("Credit Crisis", "Credit Crisis"),
+                ("Paulson Bailout", "Paulson Bailout"),
+                ("Geithner Plan", "Geithner Plan"),
+                ("Current Economics", "Current Economics"),
+                ("Currency", "Currency"),
+            ]
+        },
+    ],
+    "Test Prep": [
+        ("SAT Math", "SAT Preparation", "/sat"),
+        {
+            "GMAT": [
+                ("Problem Solving", "GMAT: Problem Solving", "/gmat"),
+                ("Data Sufficiency", "GMAT Data Sufficiency", "/gmat"),
+            ]
+        },
+        ("CAHSEE", "CAHSEE Example Problems"),
+        ("IIT JEE", "IIT JEE Questions"),
+        ("Singapore Math", "Singapore Math"),
+    ],
+    "Talks and Interviews": [
+        ("Talks and Interviews", "Khan Academy-Related Talks and Interviews")
+    ]
+}
 
 # Each DVD needs to stay under 4.4GB
 
@@ -64,58 +145,29 @@ DVDs_dict = {
     ],    
 }
 
-        
 # replace None with the DVD name above that you want to burn
 # this will restrict the homepage to only show the playlists from that list
 DVD_list = DVDs_dict.get(None) #'Math'
 
+def sorted_playlist_titles():
+    playlist_titles = []
+    append_playlist_titles(playlist_titles, PLAYLIST_STRUCTURE)
+    playlist_titles.sort()
+    return playlist_titles
+
+def append_playlist_titles(playlist_titles, obj):
+    type_obj = type(obj)
+    if type_obj == dict:
+        for key in obj:
+            append_playlist_titles(playlist_titles, obj[key])
+    elif type_obj == list:
+        for val in obj:
+            append_playlist_titles(playlist_titles, val)
+    elif type_obj == tuple:
+        playlist_titles.append(obj[1])
+
 if DVD_list:
     topics_list = all_topics_list = DVD_list
 else:
-    topics_list = []
-    topics_list.append('Arithmetic')
-    topics_list.append('Chemistry')
-    topics_list.append('Developmental Math')
-    topics_list.append('Developmental Math 2')
-    topics_list.append('Pre-algebra')
-    topics_list.append('MA Tests for Education Licensure (MTEL) -Pre-Alg')
-    topics_list.append('Geometry')
-    topics_list.append('California Standards Test: Geometry')
-    topics_list.append('Current Economics')
-    topics_list.append('Banking and Money')
-    topics_list.append('Venture Capital and Capital Markets')
-    topics_list.append('Finance')
-    topics_list.append('Credit Crisis')
-    topics_list.append('Currency')
-    topics_list.append('Valuation and Investing')
-    topics_list.append('Geithner Plan')
-    topics_list.append('Algebra')
-    topics_list.append('Algebra I Worked Examples')
-    topics_list.append('ck12.org Algebra 1 Examples')
-    topics_list.append('California Standards Test: Algebra I')
-    topics_list.append('California Standards Test: Algebra II')
-    topics_list.append('Brain Teasers')
-    topics_list.append('Biology')
-    topics_list.append('Trigonometry')
-    topics_list.append('Precalculus')
-    topics_list.append('Statistics')
-    topics_list.append('Probability')
-    topics_list.append('Calculus')
-    topics_list.append('Differential Equations')
-    topics_list.append('Khan Academy-Related Talks and Interviews')
-    topics_list.append('History')
-    topics_list.append('Organic Chemistry')
-    topics_list.append('Linear Algebra')
-    topics_list.append('Physics')
-    topics_list.append('Paulson Bailout')
-    topics_list.append('CAHSEE Example Problems')
-    topics_list.append('Cosmology and Astronomy')
-    topics_list.append('IIT JEE Questions')
-    topics_list.sort()
+    topics_list = all_topics_list = sorted_playlist_titles()
 
-    all_topics_list = copy.copy(topics_list)
-    all_topics_list.append('SAT Preparation')        
-    all_topics_list.append('GMAT: Problem Solving')
-    all_topics_list.append('GMAT Data Sufficiency')        
-    all_topics_list.append('Singapore Math')        
-    all_topics_list.sort()  
