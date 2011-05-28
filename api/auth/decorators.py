9ba56114fb6f298f2c57394a11d010ed2a10440c
Ben Kamens <ben@khanacademy.org>
2011-05-27 17:27:53 -0700

    During our provider-side OAuth validation, if we think you have a legit token but the services we consume (FB/Google) disagree about their tokens, act as if our token is now invalid.

--- api/auth/decorators.py
+++ api/auth/decorators.py
@@ -9,6 +9,8 @@ from api.auth.models import OAuthMap
 from oauth_provider.decorators import is_valid_request, validate_token
 from oauth_provider.oauth import OAuthError
 
+import util
+
 # Flask-friendly wrapper for validating an oauth request and storing the OAuthMap for use
 # in the rest of the request.
 def oauth_required(func):
@@ -23,6 +25,12 @@ def oauth_required(func):
                     # Store the OAuthMap containing all auth info in the request global
                     # for easy access during the rest of this request.
                     flask.g.oauth_map = OAuthMap.get_from_access_token(token.key_)
+
+                    if not util.get_current_user():
+                        # If our OAuth provider thinks you're logged in but the 
+                        # identity providers we consume (Google/Facebook) disagree,
+                        # we act as if our token is no longer valid.
+                        return oauth_error_response(OAuthError("Unable to get current user from oauth token."))
 
                     return func(*args, **kwargs)
 
