74a2ad138eee8454cafcf1e79a0120d7755eb0f9
Ben Kamens <ben@khanacademy.org>
2011-05-27 17:28:20 -0700

    Hide a couple more userdata properties from json serialization

--- models.py
+++ models.py
@@ -411,7 +411,7 @@ class UserData(db.Model):
     _serialize_blacklist = [
             "assigned_exercises", "badges", "count_feedback_notification",
             "last_daily_summary", "need_to_reassess", "videos_completed",
-            "moderator"
+            "moderator", "expanded_all_exercises", "question_sort_order"
     ]
     
     @staticmethod
