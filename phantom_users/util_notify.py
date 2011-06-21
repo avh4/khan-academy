d459a8007c480ddc9088c68544f891812c69d304
Parker Kuivila <ptk3@duke.edu>
2011-06-21 08:47:19 -0700

    Changed notification text

--- phantom_users/util_notify.py
+++ phantom_users/util_notify.py
@@ -15,7 +15,7 @@ import layer_cache
 import request_handler
 from badges import badges
 import logging
-
+import string
 
 def update(user,user_data,user_exercise,threshold = False, isProf = False):
     if not phantom_util.is_phantom_email(user.email()):
@@ -27,6 +27,9 @@ def update(user,user_data,user_exercise,threshold = False, isProf = False):
     # user_badges = memcache.get(badges.UserBadgeNotifier.key_for_user(user)) or []
     if user_exercise != None:
         numquest = user_exercise.total_done
+        prof = str(user_exercise.exercise)
+        prof = string.replace(prof,"_"," ")
+        prof = prof.title()
     if user_data != None:
         numbadge = user_data.badges
         user_badges = memcache.get(badges.UserBadgeNotifier.key_for_user(user)) or [] #Only allow badge notifications when earned
@@ -35,20 +38,20 @@ def update(user,user_data,user_exercise,threshold = False, isProf = False):
     
     # Every 20 questions
     if numquest != None and numquest % 20 == 0:
-        notifications.UserLoginNotifier.push_for_user(user,"You've answered "+str(numquest)+" questions so far! Login or register to save your progress")
+        notifications.UserLoginNotifier.push_for_user(user,"You've answered "+str(numquest)+" questions so far! <u>Login</u> or <u>register</u> to save your progress")
     #Proficiency
     if isProf:
-        notifications.UserLoginNotifier.push_for_user(user,"You're proficient in "+str(user_exercise.exercise)+". Login or register to save your progress")
+        notifications.UserLoginNotifier.push_for_user(user,"You're proficient in "+str(prof)+". <u>Login</u> or <u>register</u> to save your progress")
     #First Badge
     if numbadge != None and len(numbadge) == 1 and (len(user_badges) > 0):
-        notifications.UserLoginNotifier.push_for_user(user,"Congrats on your first badge! You should login or register to save your progress")
+        notifications.UserLoginNotifier.push_for_user(user,"Congrats on your first badge! You should <u>login</u> or <u>register</u> to save your progress")
     #Every 5 badges
     if numbadge != None and len(numbadge) % 5 == 0 and (len(user_badges) > 0):
-        notifications.UserLoginNotifier.push_for_user(user,"You've earned "+str(len(numbadge))+" badges so far. Have you considered logging in or registering so you don't lose your progress?")
+        notifications.UserLoginNotifier.push_for_user(user,"You've earned "+str(len(numbadge))+" badges so far. Have you considered <u>logging</u> in or <u>registering</u> so you don't lose your progress?")
     #Every 2.5k points
     if numpoint != None and threshold:
         numpoint = 2500*(numpoint/2500)+2500
-        notifications.UserLoginNotifier.push_for_user(user,"You've earned over "+str(numpoint)+ " points! If you want to keep them, you'll need to login or register.")
+        notifications.UserLoginNotifier.push_for_user(user,"You've earned over "+str(numpoint)+ " points! If you want to keep them, you'll need to <u>login</u> or <u>register</u>.")
 
     #notifications.UserLoginNotifier.push_for_user(user,"You need to login!!!")
 
