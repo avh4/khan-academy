e31219317a269dc82cdf499bd3f47ae6577a6f2c
Ben Kamens <ben@khanacademy.org>
2011-06-27 11:19:05 -0700

    Access-Control-Allow-Origin for API requests

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,6 @@
 import sys, os
 import logging
+from functools import wraps
 
 from app import App
 
@@ -15,11 +16,29 @@ for filename in os.listdir(package_dir):
         sys.path.insert(0, "%s/%s" % (package_dir, filename))
 
 from flask import Flask
+from flask import current_app
+
 api_app = Flask('api')
 api_app.secret_key = App.flask_secret_key
 
 def route(rule, **options):
-    def fix_endpoint_names_for_decorated_functions(f):
-        return api_app.add_url_rule(rule, rule, f, **options)
-    return fix_endpoint_names_for_decorated_functions
+    def api_route_wrap(func):
 
+        func = allow_cross_origin(func)
+
+        # Fix endpoint names for decorated functions by using the rule for names
+        return api_app.add_url_rule(rule, rule, func, **options)
+
+    return api_route_wrap
+
+def allow_cross_origin(func):
+    @wraps(func)
+    def cross_origin_allowed(*args, **kwargs):
+        result = func(*args, **kwargs)
+
+        if isinstance(result, current_app.response_class):
+            result.headers["Access-Control-Allow-Origin"] = "*"
+
+        return result
+
+    return cross_origin_allowed
