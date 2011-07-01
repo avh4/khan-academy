5089a2b803fa74f773c4ff5f54c2d4379c00d6a6
Desmond Brand <dmnd@desmondbrand.com>
2011-07-01 14:27:00 -0700

    Better nicknames for Google users

--- nicknames.py
+++ nicknames.py
@@ -1,10 +1,9 @@
-
 import facebook_util
 
 def get_nickname_for(user):
     if facebook_util.is_facebook_email(user.email()):
-        return facebook_util.get_facebook_nickname(user)
+        nickname = facebook_util.get_facebook_nickname(user)
     else:
-        return user.nickname()
-
-
+        nickname = user.nickname().split('@')[0]
+        nickname = nickname[0].upper() + nickname[1:]
+    return nickname
