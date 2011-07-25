2603b8319690624c0a9943b75799d5adb0c7d7dd
Parker Kuivila <ptk3@duke.edu>
2011-07-25 14:35:51 -0700

    Backfill for converting @property to actual field property

--- backfill.py
+++ backfill.py
@@ -9,20 +9,26 @@ import consts
 import points
 import nicknames
 from facebook_util import is_facebook_email
+
+def user_data_properties(user_data):
+    if user_data:
+        user_data.nickname = nicknames.get_nickname_for(user_data.current_user)
+        user_data.email = user_data.current_user.email()
+        yield op.db.Put(user_data)
+
     
 class StartNewBackfillMapReduce(request_handler.RequestHandler):
     def get(self):
-        pass
         # Admin-only restriction is handled by /admin/* URL pattern
         # so this can be called by a cron job.
 
         # Start a new Mapper task.
-        # mapreduce_id = control.start_map(
-        #         name = "BackfillExerciseOrder",
-        #         handler_spec = "backfill.order_related_videos",
-        #         reader_spec = "mapreduce.input_readers.DatastoreInputReader",
-        #         reader_parameters = {"entity_kind": "models.Exercise"},
-        #         shard_count = 64,
-        #         queue_name = "backfill-mapreduce-queue",
-        #         )
-        # self.response.out.write("OK: " + str(mapreduce_id))
+        mapreduce_id = control.start_map(
+                name = "BackfillUserProperties",
+                handler_spec = "backfill.user_data_properties",
+                reader_spec = "mapreduce.input_readers.DatastoreInputReader",
+                reader_parameters = {"entity_kind": "models.UserData"},
+                shard_count = 64,
+                queue_name = "backfill-mapreduce-queue",
+                )
+        self.response.out.write("OK: " + str(mapreduce_id))
