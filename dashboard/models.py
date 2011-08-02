1185f8b5126a532c843a83049fcc38da0e6de6e8
Ben Kamens <ben@khanacademy.org>
2011-08-01 22:11:34 -0700

    Joel's code review

--- dashboard/models.py
+++ dashboard/models.py
@@ -24,7 +24,7 @@ class DailyStatistic(object):
         stat_name = self.__class__.__name__
 
         if stat_name == DailyStatistic.__name__:
-            raise Exception("Not implemented")
+            raise Exception("DailyStatistic cannot be used directly. Must use an implementing subclass.")
 
         return stat_name
     
