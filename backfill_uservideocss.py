c5b57dd023ea58016cf0e1303775a5bd24bf928c
Ben Kamens <ben@khanacademy.org>
2011-08-04 00:14:26 -0700

    Couple safety checks to help CSS backfill finish

--- backfill_uservideocss.py
+++ backfill_uservideocss.py
@@ -5,11 +5,15 @@ from mapreduce import control
 from mapreduce import operation as op
 
 from models import UserData, UserVideo, UserVideoCss
+from google.appengine.ext.db import ReferencePropertyResolveError
 
 def backfill_user_data(user_data):
     batch_size = 1000
     query_results = True
     cursor = None
+
+    if not user_data or not user_data.user:
+        return
 
     user_video_css = UserVideoCss.get_for_user_data(user_data)
     css_dict = {'started': set([]), 'completed': set([])}
@@ -24,7 +28,10 @@ def backfill_user_data(user_data):
         query_results = False
         for user_video in user_video_query.fetch(batch_size):
             query_results = True
-            css_dict['completed'].add('.v'+str(user_video.video.key().id()))
+            try:
+                css_dict['completed'].add('.v'+str(user_video.video.key().id()))
+            except ReferencePropertyResolveError:
+                pass
         cursor = user_video_query.cursor()
 
     query_results = True
@@ -39,7 +46,10 @@ def backfill_user_data(user_data):
         query_results = False
         for user_video in user_video_query.fetch(batch_size):
             query_results = True
-            css_dict['started'].add('.v'+str(user_video.video.key().id()))
+            try:
+                css_dict['started'].add('.v'+str(user_video.video.key().id()))
+            except ReferencePropertyResolveError:
+                pass
         cursor = user_video_query.cursor()
 
     if len(css_dict['started']) or len(css_dict['completed']):
