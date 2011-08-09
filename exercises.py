ef2fb6c59b6886b0fb49a93a4bb1b12fb32b8683
Joel Burget <joelburget@gmail.com>
2011-08-09 11:04:12 -0700

    Rudimentary support for more detailed reporting.

--- exercises.py
+++ exercises.py
@@ -111,6 +111,40 @@ class ViewExercise(request_handler.RequestHandler):
                 # We cannot render old problems that were created in the v1 exercise framework.
                 renderable = False
 
+            query = models.ProblemLog.all()
+            query.filter("user = ", user_data.user)
+            query.filter("exercise = ", exid)
+            query.filter("problem_number = ", problem_number)
+
+            problem_log = query.get()
+
+            user_activity = []
+
+            if problem_log:
+                if len(problem_log.attempts) == 1 and problem_log.correct:
+                    user_activity.append(["correct-activity",
+                        "Answered %s in %ds" % (str(problem_log.attempts[0]),
+                            max(0, problem_log.time_taken_attempts[0]))
+                        ])
+                else:
+                    for i in range(0, len(problem_log.attempts)):
+                        user_activity.append([
+                            "incorrect-activity",
+                            "Answered %s in %ds" % (str(problem_log.attempts[i]), 
+                                max(0, problem_log.time_taken_attempts[i]))
+                            ])
+
+                logging.critical(problem_log.hints_used)
+                if problem_log.hints_used is not None:
+                    user_exercise.hints_used = problem_log.hints_used
+            else:
+                user_activity.append([
+                    "unavailable-activity",
+                    "Activity Unavailable"
+                    ])
+
+            user_exercise.user_activity = user_activity
+
         browser_disabled = self.is_older_ie()
         renderable = renderable and not browser_disabled
 
