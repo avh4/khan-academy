0f65ff74beff873160d3a8b3f65b35cee4ceb672
Ben Kamens <ben@khanacademy.org>
2011-08-14 13:52:03 -0700

    New backfill checks and reports essential state of UserData entities. Previous bugs + partial deploys + rollbacks may have left users in a strange state.

--- backfill.py
+++ backfill.py
@@ -5,27 +5,32 @@ from mapreduce import operation as op
 
 import request_handler
 import models
-from dashboard.models import RegisteredUserCount
+import facebook_util
 
-def add_user_id(user_data):
-    if not user_data or not user_data.current_user:
+def check_user_properties(user_data):
+    if not user_data or not user_data.user:
         return
-    user = user_data.current_user
-    if user_data.user_id and user_data.user_email:
-        return
-    user_id = user.user_id()
-    if user_id:
-        user_data.user_id = "http://googleid.khanacademy.org/"+user_id
-        user_data.user_email = user.email()
-    else:
-        user_data.user_id = user_data.email
-        user_data.user_email = user.email()
 
-    yield op.db.Put(user_data)
+    if not user_data.current_user:
+        logging.critical("Missing current_user: %s" % user_data.user)
+
+    if not user_data.user_id:
+        logging.critical("Missing user_id: %s" % user_data.user)
+
+    if not user_data.user_email:
+        logging.critical("Missing user_email: %s" % user_data.user)
+
+    if user_data.current_user.email() != user_data.user_email:
+        logging.warning("current_user does not match user_email: %s" % user_data.user)
+
+    if facebook_util.is_facebook_user_id(user_data.user_id) or facebook_util.is_facebook_user_id(user_data.user_email):
+        if user_data.user_id != user_data.user_email:
+            logging.critical("facebook user's user_id does not match user_email: %s" % user_data.user)
 
 class StartNewBackfillMapReduce(request_handler.RequestHandler):
     def get(self):
-        # pass
+        pass
+
         # Admin-only restriction is handled by /admin/* URL pattern
         # so this can be called by a cron job.
 
@@ -39,13 +44,3 @@ class StartNewBackfillMapReduce(request_handler.RequestHandler):
         #     queue_name = "backfill-mapreduce-queue",
         #   )
         # self.response.out.write("OK: " + str(mapreduce_id))
-        
-        mapreduce_id = control.start_map(
-               name = "BackfillAddUserID",
-               handler_spec = "backfill.add_user_id",
-               reader_spec = "mapreduce.input_readers.DatastoreInputReader",
-               reader_parameters = {"entity_kind": "models.UserData"},
-               shard_count = 64,
-               queue_name = "backfill-mapreduce-queue",
-               )
-        self.response.out.write("OK: " + str(mapreduce_id))
