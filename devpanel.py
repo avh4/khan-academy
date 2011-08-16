0c9e7322d470f73de8a2539f59791dca3cd621b5
spicyjalapeno@gmail.com <spicyjalapeno@gmail.com>
2011-08-15 22:28:21 -0700

    Use get_from_user_input_email properly

--- devpanel.py
+++ devpanel.py
@@ -20,8 +20,8 @@ class Email(request_handler.RequestHandler):
         swap = self.request.get('swap') #Are we changing emails?
         
 
-        currdata = UserData.get_from_user_email(current_email)
-        newdata = UserData.get_from_user_email(new_email)
+        currdata = UserData.get_from_user_input_email(current_email)
+        newdata = UserData.get_from_user_input_email(new_email)
         
         if swap and currdata: #are we swapping? make sure account exists
             currdata.current_user = users.User(new_email)
@@ -43,15 +43,15 @@ class Manage(request_handler.RequestHandler):
         errormessage = ""
         add_dev = self.request.get('adddev', None) #email that is currently used 
         remove_dev = self.request.get('removedev', None) #email the user wants to change to     
-        if add_dev and not UserData.get_from_user_email(add_dev):
+        if add_dev and not UserData.get_from_user_input_email(add_dev):
             errormessage = "You can't add a user that doesn't exist!"
         
-        if remove_dev and not UserData.get_from_user_email(remove_dev):
+        if remove_dev and not UserData.get_from_user_input_email(remove_dev):
             errormessage = "You can't remove a user that doesn't exist!"
             
         
         if add_dev and errormessage == "":
-            dev = UserData.get_from_user_email(add_dev)
+            dev = UserData.get_from_user_input_email(add_dev)
             if dev.developer == True:
                 errormessage = "%s is already flagged as a developer!" % add_dev
             else:
@@ -59,7 +59,7 @@ class Manage(request_handler.RequestHandler):
                 dev.put()
     
         if remove_dev and errormessage == "":
-            dev = UserData.get_from_user_email(remove_dev)
+            dev = UserData.get_from_user_input_email(remove_dev)
             if dev.developer == True:
                 dev.developer = False
                 dev.put()
