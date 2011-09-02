00776e42604a6c287a3b2be9975beae5bb631661
David Hu <david@khanacademy.org>
2011-09-02 12:40:18 -0700

    layer_cache (even for buckets); predictable cycling; delete unused code

--- exercisestats/report_json.py
+++ exercisestats/report_json.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 
+import layer_cache
 import request_handler
 import user_util
 
@@ -12,9 +13,14 @@ import random
 import time
 import simplejson as json
 
+import logging
+
 # Constants for Geckoboard display.
 NUM_BUCKETS = 20
 PAST_DAYS_TO_SHOW = 7
+REFRESH_SECS = 10
+
+CACHE_EXPIRATION_SECS = 60 * 60
 
 # Create a new list of KVPs with the values of all KVPs with identical keys summed
 def sum_keys(key_value_pairs):
@@ -35,22 +41,66 @@ def exercises_in_bucket(num_buckets, bucket_index):
 
     # These calculations evenly distribute exercises among buckets, with excess
     # going to the first few buckets.
-    # In particular, max_capcity(buckets) - min_capacity(buckets) <= 1.
-    bucket_size = len(exercise_names) / num_buckets
-    bucket_rem = len(exercise_names) % num_buckets
-    first = bucket_index * bucket_size + min(bucket_rem, bucket_index)
-    return exercise_names[ first : first + bucket_size + (1 if bucket_index < bucket_rem else 0) ]
+    # In particular, max_capacity(buckets) - min_capacity(buckets) <= 1.
+    num_exercises = len(exercise_names)
+    min_bucket_size = num_exercises / num_buckets
+    bucket_rem = num_exercises % num_buckets
+
+    first = bucket_index * min_bucket_size + min(bucket_rem, bucket_index)
+    return exercise_names[ first : first + get_bucket_size(num_buckets, bucket_index) ]
+
+def get_bucket_size(num_buckets, bucket_index):
+    num_exercises = len(Exercise.get_all_use_cache())
+    bucket_rem = num_exercises % num_buckets
+    return (num_exercises / num_buckets) + (1 if bucket_index < bucket_rem else 0)
+
+# Choose the exercise based on the time, so we can cycle predictably and also
+# use the cache
+def get_bucket_cursor(refresh_secs, bucket_size):
+    unix_secs = int( time.mktime(dt.datetime.now().timetuple()) )
+    ret = (unix_secs / refresh_secs) % bucket_size
+    return ret
 
 class ExerciseDoneProfGraph(request_handler.RequestHandler):
-    def get(self):
+    def get_request_params(self):
         chart = self.request_string('chart', 'area_spline')
-
         past_days = self.request_int('past_days', 7)
+        num_buckets = self.request_int('n', 1)
+        bucket_index = self.request_int('ix', 0)
+        refresh_secs = self.request_int('rsecs', 30)
+
+        bucket_size = get_bucket_size(num_buckets, bucket_index)
+        bucket_cursor = get_bucket_cursor(refresh_secs, bucket_size)
+
+        return {
+            'chart': chart,
+            'past_days': past_days,
+            'num_buckets': num_buckets,
+            'bucket_index': bucket_index,
+            'bucket_cursor': bucket_cursor,
+        }
+
+    def get_cache_key(self):
+        params = self.get_request_params()
+
+        if (params['chart'] == 'aggregate'):
+            return "%s|%d" % (params['chart'], params['past_days'])
+
+        return "%s|%d|%d|%d|%d" % (params['chart'], params['past_days'],
+            params['num_buckets'], params['bucket_index'], params['bucket_cursor'])
+
+    def get(self):
+        self.response.out.write(self.get_use_cache())
+
+    @layer_cache.cache_with_key_fxn(get_cache_key, expiration=CACHE_EXPIRATION_SECS)
+    def get_use_cache(self):
+        params = self.get_request_params()
+
         today = dt.date.today()
         # We don't use App Engine Query filters so as to avoid adding entries to index.yaml
-        days = [ today - dt.timedelta(days=i) for i in range(past_days - 1, -1, -1) ]
+        days = [ today - dt.timedelta(days=i) for i in range(0, params['past_days']) ]
 
-        if chart == 'aggregate':
+        if params['chart'] == 'aggregate':
             # For-loop for clarity and to get flattened list (over list comp.)
             ex_stats = []
             for ex in Exercise.get_all_use_cache():
@@ -58,19 +108,12 @@ class ExerciseDoneProfGraph(request_handler.RequestHandler):
 
             return self.area_spline(ex_stats, 'All Exercises')
 
-        num_buckets = self.request_int('n', 1)
-        bucket_index = self.request_int('ix', 0)
-        exercise_names = self.request_string('exercise_names', '')
+        exercise_names = exercises_in_bucket(params['num_buckets'], params['bucket_index'])
 
-        exercise_names = [e for e in exercise_names.split(',') if e]
-        if not exercise_names:
-            exercise_names = exercises_in_bucket(num_buckets, bucket_index)
-
-        exid = random.choice(exercise_names)
+        exid = exercise_names[params['bucket_cursor']]
         ex_stats = ExerciseStatistic.get_by_dates(exid, days)
 
-        return { 'gecko_line': self.gecko_line,
-                 'area_spline': self.area_spline }[chart](ex_stats, exid)
+        return self.area_spline(ex_stats, exid)
 
     def area_spline(self, exercise_stats, title=''):
         prof_list, done_list = [], []
@@ -103,38 +146,13 @@ class ExerciseDoneProfGraph(request_handler.RequestHandler):
             },
         }
 
-        self.render_template('exercisestats/highcharts_area_spline.json', context)
-
-    def gecko_line(self, exercise_stats, title):
-        # Acceptable values are "done" and "proficient"
-        hist = self.request_string('hist', 'done')
-
-        values, months = [], []
-        num_func = { 'done': ExerciseStatistic.num_problems_done,
-                     'proficient': ExerciseStatistic.num_proficient }[hist]
-
-        for ex in exercise_stats:
-            values.append(num_func(ex))
-            month = ex.start_dt.strftime('%b')
-            if len(months) == 0 or month != months[-1]:
-                months.append(month)
-
-        axisy = [min(values), max(values)] if len(values) else [0, 1]
-        gecko_dict = {
-            'item': values,
-            'settings': {
-                'axisx': months,
-                'axisy': axisy,
-                'colour': 'ff9900',
-            }
-        }
-
-        self.render_json(gecko_dict)
+        return self.render_template_to_string(
+            'exercisestats/highcharts_area_spline.json', context)
 
 # This redirect is to eliminate duplicate code so we don't have to change every
-# Geckoboard widget's URL for a general change
+# Geckoboard widgets' URL for a general change
 class GeckoboardExercisesRedirect(request_handler.RequestHandler):
     def get(self):
         bucket_index = self.request_int('ix', 0)
-        return self.redirect('/exercisestats/json?chart=area_spline&past_days=%d&n=%d&ix=%d'
-            % (PAST_DAYS_TO_SHOW, NUM_BUCKETS, bucket_index))
+        return self.redirect('/exercisestats/json?chart=area_spline&past_days=%d&rsecs=%d&n=%d&ix=%d'
+            % (PAST_DAYS_TO_SHOW, REFRESH_SECS, NUM_BUCKETS, bucket_index))
