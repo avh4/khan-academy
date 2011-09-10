d4cac918ef934895b98f2420fc3dd4174fcccdf5
Ben Kamens <ben@khanacademy.org>
2011-09-10 14:38:43 -0700

    Fleshing out unrun tests

--- gae_bingo/tests/__init__.py
+++ gae_bingo/tests/__init__.py
@@ -1,16 +1,21 @@
 import logging
+import json
 
 from google.appengine.ext.webapp import template, RequestHandler
 
+from gae_bingo.gae_bingo import ab_test, bingo
 from gae_bingo.cache import BingoCache
 from gae_bingo.config import can_control_experiments
+from gae_bingo.dashboard import ControlExperiment
 
 class RunStep(RequestHandler):
 
     def get(self):
 
+        if not os.environ["SERVER_SOFTWARE"].startswith('Development'):
+            return
+
         if not can_control_experiments():
-            self.redirect("/")
             return
 
         step = self.request.get("step")
@@ -18,10 +23,22 @@ class RunStep(RequestHandler):
 
         if step == "delete_all":
             v = self.delete_all_experiments()
-        elif step == "participate_in_A":
+        elif step == "participate_in_monkeys":
             v = self.participate_in_monkeys()
+        elif step == "participate_in_gorillas":
+            v = self.participate_in_monkeys()
+        elif step == "participate_in_chimpanzees":
+            v = self.participate_in_chimpanzees()
+        elif step == "convert_in":
+            v = self.convert_in()
+        elif step == "count_participants_in":
+            v = self.count_participants_in()
+        elif step == "count_conversions_in":
+            v = self.count_conversions_in()
+        elif step == "count_experiments":
+            v = self.count_experiments()
 
-        self.response.out.write(v)
+        self.response.out.write(json.dumps(v))
 
     def delete_all_experiments(self):
 
@@ -34,3 +51,40 @@ class RunStep(RequestHandler):
 
     def participate_in_monkeys(self):
         return ab_test("monkeys")
+
+    def participate_in_gorillas(self):
+        return ab_test("gorillas", ["a", "b", "c"])
+
+    def participate_in_chimpanzees(self):
+        return ab_test("chimpanzees", conversion_name=["chimps_conversion_1, chimps_conversion_2"])
+
+    def participate_in_crocodiles(self):
+        # Weighted test
+        return ab_test("crocodiles", {"a": 100, "b": 200, "c": 400, "d": 800, "e": 1600})
+
+    def convert_in(self):
+        bingo(self.request.get("conversion_name"))
+        return True
+
+    def end_and_choose(self):
+        bingo_cache = BingoCache.get()
+        experiment = bingo_cache.get_experiment(self.request.get("experiment_name"))
+        ControlExperiment.choose_alternative(self, experiment)
+
+    def count_participants_in(self):
+        return reduce(lambda a, b: a + b, 
+                map(lambda alternative: alternative.participants, 
+                    BingoCache.get().get_alternatives(self.request.get("experiment_name"))
+                    )
+                )
+
+    def count_conversions_in(self):
+        dict_conversions = {}
+
+        for alternative in BingoCache.get().get_alternatives(self.request.get("experiment_name")):
+            dict_conversions[alternative.number] = alternative.conversions
+
+        return dict_conversions
+
+    def count_experiments(self):
+        return len(BingoCache.get().experiments)
--- gae_bingo/tests/run_tests.py
+++ gae_bingo/tests/run_tests.py
@@ -1,33 +1,73 @@
 
-# Delete all experiments (response should be "0")
-#
-# Participate in experiment A N times, check for correct alternative values being returned, identifying as a bot once or twice (response should be "True" or "False")
-#
-# Check total participants in A
-#
-# Participate in experiment B once or twice
-#
-# Check experiment and alternative creation
-#
-# Participate in experiment A M more times, using cookies some of the time to maintain identity
-#
-# Check total participants in A
-#
-# Participate and convert in experiment A O times, using cookies some of the time, tracking conversions-per-alternative
-#
-# Check total conversions-per-alternative in A
-#
-# Participate in experiment B N times, using cookies to maintain identity
-#
-# Make sure alternatives for B are stable per identity
-#
-# Participate in experiment C N times, which is a multi-conversion experiment
-#
-# Convert in *one* of C's conversions a couple times
-#
-# Make sure conversion counts are correct for both of C's conversion experiments
-#
-# End experiment C, choosing a short-circuit alternative
-#
-# Make sure short-circuited alternatives for both of C's experiments are set appropriately
+def run_tests():
+
+    # Delete all experiments (response should be count of experiments left)
+    assert(test_response("delete_all") == 0)
+
+    # Participate in experiment A, check for correct alternative values being returned,
+    for i in range(0, 20):
+        assert(test_response("participate_in_monkeys") in [True, False])
+
+    # Identify as a bot a couple times (response should stay the same)
+    bot_value = None
+    for i in range(0, 5):
+        value = test_response("participate_in_monkeys", bot=True)
+
+        if bot_value is None:
+            bot_value = value
+
+        assert(value == bot_value)
+
+    # Check total participants in A (1 extra for bots)
+    assert(test_response("count_participants_in", {"experiment_name": "monkeys"}) == 21)
+    
+    # Participate in experiment B (responses should be "a" "b" or "c")
+    for i in range(0, 15):
+        assert(test_response("participate_in_gorillas") in ["a", "b", "c"])
+
+    # Participate in experiment A, using cookies half of the time to maintain identity
+    for i in range(0, 20):
+        assert(test_response("participate_in_monkeys", use_last_cookies=(i % 2 == 0)) in [True, False])
+
+    # Check total participants in A
+    assert(test_response("count_participants_in", {"experiment_name": "monkeys"}) == 31)
+
+    # Participate and convert in experiment A, using cookies to tie participation to conversions,
+    # tracking conversions-per-alternative
+    dict_conversions = {}
+    for i in range(0, 35):
+        alternative = test_response("participate_in_monkeys")
+        assert(test_response("convert_in", {"conversion_name": "monkeys"}, use_last_cookies=True) == True)
+
+        if not alternative.number in dict_conversions:
+            dict_conversions[alternative.number] = 0
+        dict_conversions[alternative.number] += 1
+
+    # Check total conversions-per-alternative in A
+    assert(35 == reduce(lambda a, b: a + b, map(lambda key: dict_conversions[key], dict_conversions)))
+    assert(test_response("count_conversions_in", {"experiment_name": "monkeys"}) == dict_conversions)
+
+    # Participate in experiment B N times, using cookies to maintain identity
+    #
+    # Make sure alternatives for B are stable per identity
+    #
+    # Participate in experiment C N times, which is a multi-conversion experiment
+    #
+    # Convert in *one* of C's conversions a couple times
+    #
+    # Make sure conversion counts are correct for both of C's conversion experiments
+    #
+    # End experiment C, choosing a short-circuit alternative
+    #
+    # Make sure short-circuited alternatives for both of C's experiments are set appropriately
+    #
+    # Participate in experiment D N times, keeping track of alternative returned count.
+    #
+    # Make sure weighted alternatives work -> should be a < b < c < d < e, but they should all exist.
+    #
+    # Check experiments count
+
+if __name__ == "__main__":
+    run_tests()
+
 
