81074a3ee8530df3c6120f23943692d3c0715443
spicyjalapeno@gmail.com <spicyjalapeno@gmail.com>
2011-09-13 10:59:36 -0400

    Maybe now I'll be able to see /gae_bingo/dashboard

--- gae_bingo/config.py
+++ gae_bingo/config.py
@@ -7,7 +7,7 @@ from google.appengine.api import users
 # to the experiment dashboard.
 def can_control_experiments():
     from models import UserData
-    user_data = UserData.current()
+    user_data = UserData.current(bust_cache=True)
     return users.is_current_user_admin() or (user_data and user_data.developer)
 
 # CUSTOMIZE current_logged_in_identity to make your a/b sessions
