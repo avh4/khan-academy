b79f6282e762572cd4d1ca7eb1e4dbdff967f2e2
Jason Rosoff <jason@khanacademy.org>
2011-09-12 22:56:53 -0700

    Templatize social/share buttons

---
+++ social/templatetags.py
@@ -0,0 +1,86 @@
+import os
+import logging
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+
+from urllib import urlencode
+
+from templateext import escapejs
+
+import template_cached
+register = template_cached.create_template_register()
+_site_tagline = "Trying to make a world-class education available to anyone, anywhere."
+
+@register.inclusion_tag("social/facebook_share.html")
+def facebook_share_badge(desc, icon, extended_desc, activity, event_info=None):
+    context = {}
+    if desc and icon and extended_desc:
+        context = { 'type': 'badge', 
+                    'desc': desc, 
+                    'icon': icon, 
+                    'extended_desc': extended_desc,
+                    'activity': activity, 
+                    'event_info': event_info }
+    return context
+
+@register.inclusion_tag("social/facebook_share.html")
+def facebook_share_video(name, desc, youtube_id, event_info=None):
+    context = {}
+    if name and desc and id:
+        context = { 'type': 'video',
+                    'name': name,
+                    'desc': desc,
+                    'id': youtube_id,
+                    'event_info': event_info }
+    return context
+
+@register.inclusion_tag("social/facebook_share.html")
+def facebook_share_exercise(problem_count,plural,proficiency,name):
+    context = {}
+    if problems and name:
+        context = { 'type': 'exercise',
+                    'problem_count': problem_count, 
+                    'plural': plural,
+                    'proficiency': ("to achieve proficiency in" if proficiency else "in"),
+                    'name': name,
+                    'event_info': event_info }
+    return context
+    
+@register.inclusion_tag("social/twitter_share.html")
+def twitter_share_video(title, youtube_id, event_info=None):
+    context = {}
+    if title and youtube_id :
+        url = "http://khanacademy.org/video?v=%s" % youtube_id
+        text = "just learned about %s" % title 
+        context = { 'type': 'video',
+                    'url': url,
+                    'text': text,
+                    'tagline': _site_tagline, 
+                    'event-info': event_info } 
+    return context
+    
+@register.inclusion_tag("social/twitter_share.html")
+def twitter_share_badge(desc, activity, event_info=None):
+    context= {}
+    if desc:
+        url = "http://khanacademy.org"
+        text = "just earned the %s%s on" % (desc, ( "" if not activity else " in " + activity ))
+        context = { 'type': 'badge',
+                    'url': url,
+                    'text': text,
+                    'tagline': _site_tagline,
+                    'event-info': event_info }
+    return context
+
+@register.inclusion_tag("social/twitter_share.html")
+def twitter_share_exercise(name, problems, plural, proficiency, event_info=None):
+    context = {}
+    if name and problems and plural:
+        url = "http://khanacademy.org/exercisedashboard"
+        text = "just answered %s question%s right %s %s" % (problems, plural, ( "to achieve proficiency in" if proficiency else "in" ), name)
+        context = { 'url': url, 
+                    'text': text,
+                    'tagline': _site_tagline,
+                    'event_info': event_info }
+    return context
