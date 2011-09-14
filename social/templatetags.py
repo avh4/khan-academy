0168abb0b4c9d6a14f325a99e1cd05e6d8034b33
Jason Rosoff <jason@khanacademy.org>
2011-09-14 10:57:45 -0700

    Add email sharing and un-eff positioning of the share popup

--- social/templatetags.py
+++ social/templatetags.py
@@ -11,7 +11,11 @@ from templateext import escapejs
 
 import template_cached
 register = template_cached.create_template_register()
+
 SITE_TAGLINE = "Trying to make a world-class education available to anyone, anywhere."
+BASE_VIDEO_URL = "http://khanacademy.org/video?v=%s"
+BASE_EXERCISE_URL = "http://khanacademy.org/exercisedashboard"
+BASE_BADGE_URL = "http://khanacademy.org/"
 
 @register.inclusion_tag("social/facebook_share.html")
 def facebook_share_badge(desc, icon, extended_desc, activity, event_description=None):
@@ -52,7 +56,7 @@ def facebook_share_exercise(problem_count, proficiency, name, event_description=
 def twitter_share_video(title, youtube_id, event_description=None):
     context = {}
     if title and youtube_id :
-        url = "http://khanacademy.org/video?v=%s" % youtube_id
+        url = BASE_VIDEO_URL % youtube_id
         text = "just learned about %s" % title 
         context = { 'type': 'video',
                     'url': url,
@@ -65,7 +69,7 @@ def twitter_share_video(title, youtube_id, event_description=None):
 def twitter_share_badge(desc, activity, event_description=None):
     context= {}
     if desc:
-        url = "http://khanacademy.org"
+        url = BASE_BADGE_URL
         text = "just earned the %s%s on" % (desc, ( "" if not activity else " in " + activity ))
         context = { 'type': 'badge',
                     'url': url,
@@ -78,7 +82,7 @@ def twitter_share_badge(desc, activity, event_description=None):
 def twitter_share_exercise(name, problems, proficiency, event_description=None):
     context = {}
     if name and problems:
-        url = "http://khanacademy.org/exercisedashboard"
+        url = BASE_EXERCISE_URL
         text = "just answered %s question%s right %s %s" % (problems, pluralize(problems), ( "to achieve proficiency in" if proficiency else "in" ), name)
         context = { 'url': url, 
                     'text': text,
@@ -86,6 +90,39 @@ def twitter_share_exercise(name, problems, proficiency, event_description=None):
                     'event_description': event_description }
     return context
 
+@register.inclusion_tag("social/email_share.html")
+def email_share_video(title, description, youtube_id, event_description=None):
+    contex = {}
+    if title and youtube_id:
+        subject = "I just learned about %s on Khan Academy" % title
+        body = "You can learn about it too. Check out %s" % ( BASE_VIDEO_URL % youtube_id )
+        context = { 'subject': subject,
+                    'body': body,
+                    'event_description': event_description }
+    return context
+
+@register.inclusion_tag("social/email_share.html")
+def email_share_badge(desc, activity, event_description=None):
+    contex = {}
+    if desc:
+        subject = "I just earned the %s %s on Khan Academy" % (desc, ( "" if not activity else "in " + activity ))
+        body = "You should check it out %s" % BASE_BADGE_URL
+        context = { 'subject': subject,
+                    'body': body,
+                    'event_description': event_description }
+    return context
+    
+@register.inclusion_tag("social/email_share.html")
+def email_share_exercise(name, problems, proficiency, event_description=None):
+    contex = {}
+    if name and problems:
+        subject = "I was just working on about %s on Khan Academy" % name
+        body = "And I answered %s question%s right %s You can try it too: %s" % (problems, pluralize(problems), ( "to earn proficiency!" if proficiency else "." ), BASE_EXERCISE_URL)
+        context = { 'subject': subject,
+                    'body': body,
+                    'event_description': event_description }
+    return context
+    
 @register.inclusion_tag("social/share_button.html")
 def share_video_button(video_title, description, youtube_id, event_description=None):
     context = {}
