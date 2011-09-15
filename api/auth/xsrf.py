aff89b4d95203da937c88fc0f3a3e6e29e50fe9d
Ben Kamens <ben@khanacademy.org>
2011-09-14 17:06:10 -0700

    unwanted logging

--- api/auth/xsrf.py
+++ api/auth/xsrf.py
@@ -33,6 +33,5 @@ def validate_xsrf_value():
     return header_value and header_value == get_xsrf_cookie_value()
 
 def render_xsrf_js():
-    logging.critical("IN HERE!")
     return "<script>var fkey = '%s';</script>" % get_xsrf_cookie_value();
 
