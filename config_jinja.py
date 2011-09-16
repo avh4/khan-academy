9fc892f64813afe2d4d5c2e0d6aca455d47c8c9f
Ben Kamens <ben@khanacademy.org>
2011-09-16 00:15:10 -0700

    More compilation/template reload settings

--- config_jinja.py
+++ config_jinja.py
@@ -24,8 +24,9 @@ from models import UserData
 jinja2.default_config = {
     "template_path": "templates", 
     "compiled_path": "compiled_templates.zip",
-    "force_compiled": True, #not App.is_dev_server, 
-    "cache_size": 0 if App.is_dev_server else -1,
+    "force_compiled": True, #not App.is_dev_server, # Only use compiled templates in production 
+    "cache_size": 0 if App.is_dev_server else -1, # Only cache in production
+    "auto_reload": App.is_dev_server, # Don't check for template updates in production
     "globals": {
         "css_package": css_package,
         "js_package": js_package,
