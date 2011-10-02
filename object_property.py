98df313f6f96c46ca0f7c1d43227e3b45fad40bc
Desmond Brand <dmnd@desmondbrand.com>
2011-10-01 23:21:08 -0700

    Make some StringListProperties forward-compatible with a new tsv format

--- object_property.py
+++ object_property.py
@@ -29,3 +29,38 @@ class UnvalidatedObjectProperty(ObjectProperty):
         # pickle.dumps can be slooooooow,
         # sometimes we just want to trust that the item is pickle'able.
         return value
+
+
+class TsvProperty(db.UnindexedProperty):
+    '''
+    An alternative to StringListProperty that serializes lists using a simple
+    tab-separated format. This is much faster than StringPropertyList, however
+    elements with tabs are not permitted.
+    '''
+    data_type = list
+
+    def get_value_for_datastore(self, model_instance):
+        value = super(TsvProperty, self).get_value_for_datastore(model_instance)
+        return db.Text("\t".join(value))
+
+    def make_value_from_datastore(self, value):
+        return value.split("\t")
+
+# the following properties are useful for migrating StringListProperty to
+# the faster TsvProperty
+
+class TsvCompatStringListProperty(db.StringListProperty):
+    'A StringListProperty that can also lists serialized as read tab separated strings'
+    def make_value_from_datastore(self, value):
+        if isinstance(value, list):
+            return value
+        else:
+            return value.split("\t")
+
+class StringListCompatTsvProperty(TsvProperty):
+    'A TsvProperty that can also read lists serialized as native Python lists'
+    def make_value_from_datastore(self, value):
+        if isinstance(value, list):
+            return value
+        else:
+            return value.split("\t")
