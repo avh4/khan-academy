5b35196023d207362cc299e153a61e76a79238e4
Marcos Ojeda <subliminal@gmail.com>
2011-10-05 18:22:43 -0700

    Backed out changeset 22b07cee40d0 blotter is now somewhere in github and not here

--- gae_bingo/main.py
+++ gae_bingo/main.py
@@ -4,13 +4,11 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 from gae_bingo import cache
 from gae_bingo import dashboard
 from gae_bingo import middleware
-from gae_bingo import blotter
 
 application = webapp.WSGIApplication([
     ("/gae_bingo/persist", cache.PersistToDatastore),
     ("/gae_bingo/dashboard", dashboard.Dashboard),
     ("/gae_bingo/dashboard/control_experiment", dashboard.ControlExperiment),
-    ("/gae_bingo/blotter", blotter.Blotter),
 ])
 application = middleware.GAEBingoWSGIMiddleware(application)
 
